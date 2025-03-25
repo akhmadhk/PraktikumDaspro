0d62756f6f6f80ade068ed962b7e38637c412c6a
akhmadhk <154281378+akhmadhk@users.noreply.github.com>
2025-03-26 01:17:06 +0700

    Add files via upload

--- Praktikum06/LatihanFlowchartTokoBuku03.java
+++ Praktikum06/LatihanFlowchartTokoBuku03.java
@@ -3,45 +3,59 @@ import java.util.Scanner;
 
 public class LatihanFlowchartTokoBuku03 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        String hari, jenisBuku;
+        String jenisBuku, hari;
         int jumlahBuku;
-        float diskon = 0, totalDiskon = 0;
+        double diskon = 0;
 
-        System.out.print("Masukkan hari: ");
-        hari = scanner.nextLine();
+        // Input hari
+        System.out.print("Hari apa ini: ");
+        hari = sc.nextLine();
 
+        System.out.print("Masukkan jenis buku (kamus/novel/lainnya): ");
+        jenisBuku = sc.nextLine();
+
+        System.out.print("Masukkan jumlah buku: ");
+        jumlahBuku = sc.nextInt();
+
+        sc.close();
+        
+        // Diskon hari Rabu
         if (hari.equalsIgnoreCase("rabu")) {
-            System.out.print("Masukkan jenis buku: ");
-            jenisBuku = scanner.nextLine();
-            System.out.print("Masukkan jumlah buku: ");
-            jumlahBuku = scanner.nextInt();
+            // Menghitung diskon buku yang dibeli
 
+            // Diskon kamus
             if (jenisBuku.equalsIgnoreCase("kamus")) {
-                diskon = 10;
+                diskon = 0.1;
                 if (jumlahBuku > 2) {
-                    diskon += 2;
+                    diskon += 0.2;
                 }
+            // Diskon novel
             } else if (jenisBuku.equalsIgnoreCase("novel")) {
-                diskon = 7;
+                diskon = 0.07;
                 if (jumlahBuku > 3) {
-                    diskon += 2;
+                    diskon += 0.02;
+                } else if (jumlahBuku <= 3) {
+                    diskon += 0.01;
                 }
+            // Diskon buku lainnya
             } else {
-                if (jumlahBuku > 3) {
-                    diskon = 5;
+                if (jumlahBuku >= 3) {
+                    diskon += 0.05;
                 } else {
                     System.out.println("Tidak mendapatkan diskon");
                 }
             }
+            
+            // Output hasil perhitungan
+            // Agar jumlah persen yang ditampilkan tidak aneh
+            double diskonPersen = Math.round(diskon * 100 * 100.0) / 100.0;
+            System.out.println("Total diskon: " + diskonPersen + "%");
         } else {
-            System.out.println("Tidak mendapatkan diskon");
+            System.out.println("Bukan hari Rabu, tidak ada diskon");
         }
 
-        totalDiskon = diskon;
-        System.out.println("Total diskon: " + totalDiskon + "%");
-
-        scanner.close();
+        sc.close();
     }
 }
